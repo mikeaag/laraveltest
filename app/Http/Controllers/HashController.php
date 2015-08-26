4e00c143f0f37fbc0effdafff0e017bafcb7e9c4
mikeaag <mike.gatward@xenonsoft.co.uk>
2015-08-26 21:11:54 +0100

    Hash value using facade

--- app/Http/Controllers/HashController.php
+++ app/Http/Controllers/HashController.php
@@ -2,11 +2,12 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Hash;
 
 class HashController extends Controller
 {
   public function hash($value){
-    return view('hash')->with('hashvalue', $value);
+    return view('hash')->with('hashvalue', Hash::make($value));
   }
 
 }
