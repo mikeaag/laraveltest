fe3026b24dfa5c116311d5b1fa1bd1c723691a1d
mikeaag <mike.gatward@xenonsoft.co.uk>
2015-08-26 21:10:20 +0100

    Im stupid

--- app/Http/Controllers/HashController.php
+++ app/Http/Controllers/HashController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 
-class AuthController extends Controller
+class HashController extends Controller
 {
   public function hash($value){
     return view('hash')->with('hashvalue', $value);
