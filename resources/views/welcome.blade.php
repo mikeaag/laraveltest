f43fe2ce79ad2e61cc998a68931e7e2b558436a4
mikeaag <mike.gatward@xenonsoft.co.uk>
2015-08-26 20:52:55 +0100

    testing quick deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <p>Testing!</p>
             </div>
         </div>
     </body>
