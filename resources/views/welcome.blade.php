e4e091f447ab1d555fb24f7311171e3afed17d8a
mikeaag <mike.gatward@xenonsoft.co.uk>
2015-08-28 13:32:50 +0100

    Server Name!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,7 @@
                 <div class="title">Laravel 5</div>
                 <p>Testing!</p>
                 <p>More Testing</p>
+                <p><strong>Server:</strong> {!! $_SERVER['SERVER_NAME'] !!}</p>
             </div>
         </div>
     </body>
